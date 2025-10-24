58296b5c7edbd4159e61671db2eaa414f58efa03
Nguyễn Phước Bảo Hưng <baohung4052004@gmail.com>
2025-10-24 22:33:08 +0700

    add calendar component

---
+++ src/pages/Manager/Dashboard/ManagerDashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "../../../components/calander/Calendar";
+
+const ManagerDashboard: React.FC = () => {
+  return (
+    <div className="manager-dashboard">
+      <Calendar />
+    </div>
+  );
+};
+
+export default ManagerDashboard;
